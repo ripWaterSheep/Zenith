5576b71bd7cbfd7904b7de735d6dbee09bb14dcf
Neil Mehra <45741682+AsianKoala@users.noreply.github.com>
2020-01-13 16:54:43 -0500

    added world, level, and frame

--- src/Game/Components/Level.java
+++ src/Game/Components/Level.java
@@ -1,4 +1,26 @@
 package game.components;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Level {
+
+
+    public Level(String name, ImageIcon menuImage, ArrayList<World> worlds) {
+        this.name = name;
+        this.menuImage = menuImage;
+        this.worlds = worlds;
+    }
+
+
+    private String name;
+
+    private ImageIcon menuImage;
+
+    private ArrayList<World> worlds;
+
+
+    private World currWorld;
+
+    public World neededWorld() { return currWorld; }
 }
--- src/Game/Components/World.java
+++ src/Game/Components/World.java
@@ -1,4 +1,39 @@
 package game.components;
 
+import util.geometry.Point;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class World {
+
+    public World(String name, ImageIcon backgroundImage, Color borderColor, Point widthHeight, ArrayList<? extends Component> allComponents) {
+        this.name = name;
+        this.backgroundImage = backgroundImage;
+        this.borderColor = borderColor;
+        this.widthHeight = widthHeight;
+
+
+
+    }
+
+
+
+    private String name;
+
+    private ImageIcon backgroundImage;
+
+    private Color borderColor;
+
+    /**
+     * x = width
+     * y = height
+     */
+    private Point widthHeight;
+
+
+
+
+
 }
